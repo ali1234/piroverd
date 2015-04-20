c39eace41d90dd260bca86984e7cd5831b2ca8c2
Alistair Buxton <a.j.buxton@gmail.com>
2015-04-20 20:11:00 +0000

    Lights.

--- sn3218.c
+++ sn3218.c
@@ -28,6 +28,8 @@ int sn3218_open(int adapter, int address)
     if (device == -1)
         return -1;
 
+    i2c_smbus_write_byte_data(device, 0, 1);
+
     return device;
 }
 
@@ -36,3 +38,19 @@ void sn3218_close(int device)
     i2c_close(device);
 }
 
+void sn3218_set_light(int device, int light, int brightness)
+{
+    if (brightness > 255) brightness = 255;
+    else if (brightness < 0) brightness = 0;
+
+    i2c_smbus_write_byte_data(device, light+1, brightness);
+    int reg = 19 + (light / 6);
+    int bit = 1 << (light % 6);
+    __u8 ctrl = i2c_smbus_read_byte_data(device, reg);
+    if (brightness == 0) ctrl &= ~bit;
+    else ctrl |= bit;
+
+    i2c_smbus_write_byte_data(device, reg, ctrl);
+    i2c_smbus_write_byte_data(device, 22, 0);
+
+}
---
+++ sn3218.h
@@ -0,0 +1,15 @@
+#define HEADLIGHT_RIGHT_OUTER  0
+#define HEADLIGHT_RIGHT_INNER  1
+#define INDICATOR_RIGHT_FRONT  2
+#define INDICATOR_RIGHT_REAR   3
+#define BRAKELIGHT_RIGHT       4
+#define BRAKELIGHT_LEFT       13
+#define INDICATOR_LEFT_REAR   14
+#define INDICATOR_LEFT_FRONT  15
+#define HEADLIGHT_LEFT_INNER  16
+#define HEADLIGHT_LEFT_OUTER  17
+
+
+int sn3218_open(int, int);
+void sn3218_close(int);
+void sn3218_set_light(int, int, int);
